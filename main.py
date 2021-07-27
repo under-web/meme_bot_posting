747b39db784ad49f2ced90a9fcad392e572221cd
Artem <furens@protonmail.com>
2021-07-27 21:58:46 +0300

    refactor with sqlite3

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import time
 import requests
 import telebot
+import sqlite3
+from sqlite3 import Error
 from bs4 import BeautifulSoup
 from config import api_token, channel
 
 url = 'https://topmemas.top/'
 bot = telebot.TeleBot(api_token)
 
-# TODO: переписать с использованием sqlite3
+
 def get_request_html(url):
     res = requests.get(url).text
     return res
@@ -20,23 +22,22 @@ def get_id():  # парсит id для определения актуальн�
     """
     soup_id = BeautifulSoup(get_request_html(url), 'lxml').find_all('div', class_='cont_item')
     result_id = soup_id[0].get('id')
-
-    with open('id.txt', 'r', encoding='utf-8') as r_file:
-        ids = r_file.readlines()
-        if ids is None:
-            r_file.writelines(str(result_id) + '\n')
-            print('base impty, i am write')
-            return False
-        else:
-            if str(result_id) + '\n' in ids:
-                print(f'id - {result_id} in base!')
-                return True
-            else:
-                with open('id.txt', 'a', encoding='utf-8') as file:
-                    file.writelines(str(result_id) + '\n')
-                print(f' id {result_id} not in base i am write!')
-                return False
-
+    return result_id
+    # with open('id.txt', 'r', encoding='utf-8') as r_file:
+    #     ids = r_file.readlines()
+    #     if ids is None:
+    #         r_file.writelines(str(result_id) + '\n')
+    #         print('base impty, i am write')
+    #         return False
+    #     else:
+    #         if str(result_id) + '\n' in ids:
+    #             print(f'id - {result_id} in base!')
+    #             return True
+    #         else:
+    #             with open('id.txt', 'a', encoding='utf-8') as file:
+    #                 file.writelines(str(result_id) + '\n')
+    #             print(f' id {result_id} not in base i am write!')
+    #             return False
 
 
 def grab_top_meme():
@@ -51,22 +52,37 @@ def grab_top_meme():
         return image_bytes
 
 
-def main():
-    count = 1
-    while True:
-        if not get_id():
-            try:
-                bot.send_photo(channel, grab_top_meme())
-                print(f'отправил в канал {count} сообщение')
-                count += 1
-                time.sleep(600)
-            except Exception as e:
-                print(e)
-                time.sleep(30)
-        else:
-            print('ждем новых мемов')
+def sql_connection(result_id):
+    try:
+        con = sqlite3.connect('id_database.db')
+
+        cursor = con.cursor()
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS id_picture (id TEXT)""")
+        con.commit()
+
+        cursor.execute("SELECT id FROM id_picture")
+
+        if cursor.fetchone() is None:
+            cursor.execute("INSERT INTO id_picture VALUES (?)", (result_id,))
+            con.commit()
+            bot.send_photo(channel, grab_top_meme())
+            print(f'отправил сообщение')
             time.sleep(600)
-            continue
+        else:
+            print('Такая запись уже есть')
+            time.sleep(600)
+    except Error:
+        print(Error)
+    finally:
+        con.close()
+
+
+def main():
+
+    while True:
+        sql_connection(get_id())
+        continue
 
 
 if __name__ == '__main__':
